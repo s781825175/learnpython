1961c28f6da351a8558682a541654ed1c6ea65cb
s781825175 <781825175@qq.com>
2017-08-01 17:50:20 +0800

    change4

--- test4.py
+++ test4.py
@@ -1,13 +1,14 @@
 #将一个正整数分解质因数。例如：输入90,打印出90=2*3*3*5
 
-a = int(raw_input('input a number'))
-print a,'=',
+a = int(input('input a number:'))
+print (a,'=', end=' ')
 while (a!=1):
+    a=int(a)
     for i in range(2,a+1):
         if a%i==0 and a!=i:
-            print i,'*',
+            print (i,'*', end=' ')
             a=a/i
             break
         if a%i==0 and a==i:
-            print i,
+            print (i, end=' ')
             a=1
