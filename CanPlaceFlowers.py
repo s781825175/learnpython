83cd4d02e7bd2f3302264065213eb66c7ee990ab
s781825175 <781825175@qq.com>
2017-08-21 18:01:49 +0800

    170821

---
+++ CanPlaceFlowers.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        a=0
+        for i in range(1,len(flowerbed)-1):
+            if flowerbed[i]==flowerbed[i-1] and flowerbed[i]==flowerbed[i+1] and flowerbed[i]==0:
+                a+=1
+                flowerbed[i]=1
+                print(a)
+        return (a>=1)
+flowerbed = [1,0,0,0,1]
+n = 1
+print(Solution().canPlaceFlowers(flowerbed, n)
