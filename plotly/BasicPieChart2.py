60080e8245a22dee41cbf78a99ab60184ad63815
s781825175 <781825175@qq.com>
2017-10-23 18:00:06 +0800

    171023

---
+++ plotly/BasicPieChart2.py
@@ -0,0 +1,14 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+labels = ['Oxygen','Hydrogen','Carbon_Dioxide','Nitrogen']
+values = [4500,2500,1053,500]
+colors = ['#FEBFB3', '#E1396C', '#96D38C', '#D0F9B1']
+
+trace = go.Pie(labels=labels, values=values,
+               hoverinfo='label+percent', textinfo='value', 
+               textfont=dict(size=20),
+               marker=dict(colors=colors, 
+                           line=dict(color='#000000', width=2)))
+
+py.iplot([trace], filename='styled_pie_chart')
